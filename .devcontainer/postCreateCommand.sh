e2e5cbdcacbcc2b3c9215600d026bdf532696c62
Steeve Gaudreault-Lambert <steeveglambert@gmail.com>
2024-01-06 18:09:10 -0500

    Add npm server (#2)
    * fix devcontainer
    
    * Add docker compose

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y xdg-utils
-
 sudo npm install -g npm@latest @angular/cli
-npm install
+echo "source <(ng completion script)" >> /home/node/.bashrc
+
+yarn install
