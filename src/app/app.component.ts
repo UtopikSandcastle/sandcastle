5fe8b352bab8d84cab359f5e3c3ba5a8e1daebac
Steeve Gaudreault-Lambert <steeveglambert@gmail.com>
2024-01-24 01:32:05 +0000

    add navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { getManifest } from "@angular-architects/module-federation";
+import { Router, RouterOutlet } from '@angular/router';
 import { RouterModule } from "@angular/router";
-import { MatMenuModule } from "@angular/material/menu";
 
 import { buildRoutes } from './utils/routes';
 import { CustomManifest, CustomRemoteConfig } from './utils/config';
-import { MenuItemComponent } from './components/menu-item/menu-item.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    MatMenuModule,
     CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule,
     RouterModule,
     RouterOutlet,
-    MenuItemComponent,
+    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   title = 'sandcastle';
 
   remotes: CustomRemoteConfig[] = [];
 
   constructor(
     private router: Router) {
-      const manifest = getManifest<CustomManifest>();
-
-      // Hint: Move this to an APP_INITIALIZER 
-      //  to avoid issues with deep linking
-      const routes = buildRoutes(manifest);
-      this.router.resetConfig(routes);
-  
-      this.remotes = Object.values(manifest);
   }
 
-  // async ngOnInit(): Promise<void> {
-  //   const manifest = getManifest<CustomManifest>();
+  async ngOnInit(): Promise<void> {
+    const manifest = getManifest<CustomManifest>();
 
-  //   // Hint: Move this to an APP_INITIALIZER 
-  //   //  to avoid issues with deep linking
-  //   const routes = buildRoutes(manifest);
-  //   this.router.resetConfig(routes);
+    // Hint: Move this to an APP_INITIALIZER 
+    //  to avoid issues with deep linking
+    const routes = buildRoutes(manifest);
+    this.router.resetConfig(routes);
 
-  //   this.remotes = Object.values(manifest);
-  // }
+    this.remotes = Object.values(manifest);
+  }
 }
--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuItemComponent } from './menu-item.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('MenuItemComponent', () => {
-  let component: MenuItemComponent;
-  let fixture: ComponentFixture<MenuItemComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenuItemComponent]
+      imports: [NavbarComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(MenuItemComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
