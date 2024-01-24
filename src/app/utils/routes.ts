c38c598c21b1fe72f73c7daaae7a63f2a8a940dc
Steeve Gaudreault-Lambert <steeveglambert@gmail.com>
2024-01-24 02:47:15 +0000

    fix routes

--- src/app/utils/routes.ts
+++ src/app/utils/routes.ts
@@ -1,22 +1,32 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
-import { routes } from '../app.routes';
 import { CustomManifest } from "./config";
+import { routes } from '../app.routes';
 
 export function buildRoutes(options: CustomManifest): Routes {
-  const lazyRoutes: Routes = Object.keys(options).map(key => {
+  const lazyRoutes: Routes = [];
+
+  // Iterate through the manifest and load dynamic routes
+  for (const key of Object.keys(options)) {
     const entry = options[key];
-    return {
+
+    lazyRoutes.push({
       path: entry.routePath,
       loadChildren: () =>
         loadRemoteModule({
           type: 'manifest',
-          remoteName: key,
-          exposedModule: './routes'
+          remoteName: entry.remoteName,
+          exposedModule: entry.exposedModule
         })
-          .then(m => m['routes'])
-    }
-  });
+          .then(m => m[entry.ngModuleName])
+    });
+  }
 
-  return [...routes, ...lazyRoutes];
+  // Merge the remote routes with the dynamic routes
+  const allRoutes: Routes = [
+    ...routes,  // Add the remote route configuration
+    ...lazyRoutes     // Add the dynamically generated routes
+  ];
+
+  return allRoutes;
 }
